e1c4676a23a7a7a5a89b77259c043bae12a3afb8
hte88 <olivier.marion88@gmail.com>
2025-08-01 21:27:02 +0200

    feat: dep

--- Front/nuxt.config.ts
+++ Front/nuxt.config.ts
@@ -1,11 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/ui'],
+  modules: ['@nuxt/image', '@nuxt/ui', 'nuxt-charts'],
    runtimeConfig: {
     public: {
       BASE_URL: process.env.NUXT_BASE_URL,
     }
-  }
+  },  css: ['~/assets/css/main.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
