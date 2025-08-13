ea638d9a6bdfa965603a898b70eec6ef68881804
hte88 <olivier.marion88@gmail.com>
2025-08-13 14:17:05 +0200

    fix: cors

--- Back/app/main.py
+++ Back/app/main.py
@@ -12,7 +12,7 @@ app.include_router(router, prefix="/api")
 
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=["http://localhost:3000"],  # l'URL de ton front
+  allow_origins=["*"],  # l'URL de ton front
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
